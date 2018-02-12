76626b80ff78db380cd7a009dd4e86a3ce7152dc
Salmaan <sar228@cornell.edu>
2018-02-11 22:21:36 -0500

    added back get raw lines endpoint. only find trains updated in the last 5 minutes. added in lines to routeIds for SIR and S trains

--- src/routes/lines.js
+++ src/routes/lines.js
@@ -1,6 +1,8 @@
 const moment = require('moment');
 const config = require(__basedir + '/config');
 const { Train } = require(__basedir + '/models');
+const Trip = require(__basedir + '/trip.js');
+const { minsToSeconds, linesToRouteIds } = require(__basedir + '/utils.js');
 
 const ERRORS = {
   lineRequired: 'Line is required.',
@@ -9,17 +11,19 @@ const ERRORS = {
 };
 
 const getLines = (req, res) => {
-  const line = req.query.line;
+  let line = req.query.line;
   if (!line) return res.status(404).send(ERRORS.lineRequired);
 
-  // only find trains updated within the last minute
-  const oneMinuteAgo = moment().unix() - 120;
-  Train.find({ route: line.toUpperCase(), lastUpdated: { $gt: oneMinuteAgo } }).exec((err, results) => {
+  if (linesToRouteIds[line]) line = linesToRouteIds[line];
+  // only find trains updated within the last 5 minutes
+  const fourMinutesAgo = moment().unix() - minsToSeconds(5);
+
+  Train.find({ route: line.toUpperCase(), lastUpdated: { $gt: fourMinutesAgo } }).sort('-lastUpdated').exec((err, results) => {
     if (err) {
       config.error('Error querying for lines', err);
       return res.status(500).send({ e: err, message: ERRORS.internalServer });
     }
-    return res.set({ 'Content-Type': 'application/json; charset=utf-8' }).status(200).send(results);
+    return res.set({ 'Content-Type': 'application/json; charset=utf-8' }).status(200).send(JSON.stringify(results, null, 2));
   });
 };
 
@@ -27,17 +31,17 @@ const getLines = (req, res) => {
 const getRawLines = (req, res) => {
   const line = req.query.line;
   if (!line) return res.status(404).send(ERRORS.lineRequired);
-  return res.status(404).send(ERRORS.unsupported);
-  // load(line, false, (err, feed) => {
-  //   if (err) {
-  //     config.debug('error loading the line', err);
-  //     return res.status(404).send(err);
-  //   }
+  // return res.status(404).send(ERRORS.unsupported);
+  Trip.load({ line, parse: false }, (err, feed) => {
+    if (err) {
+      config.debug('error loading the line', err);
+      return res.status(404).send(err);
+    }
 
-  //   return res.set({ 'Content-Type': 'application/json; charset=utf-8' })
-  //     .status(200)
-  //     .send(JSON.stringify(feed, null, 2));
-  // });
+    return res.set({ 'Content-Type': 'application/json; charset=utf-8' })
+      .status(200)
+      .send(JSON.stringify(feed, null, 2));
+  });
 }
 
 module.exports = { getLines, getRawLines };
